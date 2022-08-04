762a249a7a29c3b03104e5680e8e817d1e039aac
alilas17 <1arisakusaku17@gmail.com>
2022-08-04 09:30:56 +0000

    user

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,4 +1,5 @@
 class TasksController < ApplicationController
+  before_action :require_user_logged_in, only: [:index, :show]
   before_action :set_task, only: [:show, :edit, :update, :destroy]
   
   def index
@@ -13,7 +14,7 @@ class TasksController < ApplicationController
   end
 
   def create
-    @task = Task.new(task_params)
+    @task = current_user.tasks.build(task_params)
     if @task.save
       flash[:success] = 'Task が正常に投稿されました'
       redirect_to @task
